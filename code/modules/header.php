0bc2c9211c19c4aaaddb735a4d2ce0f772e5aec0
seb <sebastien.poussard@protonmail.ch>
2019-04-11 18:27:06 +0200

    modif header

--- code/modules/header.php
+++ code/modules/header.php
@@ -1,5 +1,5 @@
 <!-- le code pour la partie haute de l'application web -->
-<div class="row">
+<div class="row border border-primary" style="margin:8px; border-radius:10px;">
   <!--  logo -->
   <div class="col-lg-2">
     <a href="index.php">
@@ -11,7 +11,7 @@
     <h3> Accueil <h3>
     <!-- Search form -->
     <div class="input-group mb-3">
-      <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="basic-addon2">
+      <input type="text" class="form-control" placeholder="Rechercher" aria-label="Recipient's username" aria-describedby="basic-addon2">
       <div class="input-group-append">
         <button class="btn btn-outline-secondary" type="button">Rechercher</button>
       </div>
@@ -19,6 +19,19 @@
   </div>
   <!-- cadre profil, se deconnecter ... -->
   <div class="col-lg-3">
-    <p> connecté en tant que ... se deconnecter ... profil ....</p>
+    <!-- sous groupe avec la bordure -->
+    <div class="row border border-primary" style="margin:8px; border-radius:10px;">
+      <p> Connecté en tant que ...</p>
+      <!-- colonne de gauche avec nom d'utilisateur et avatar -->
+      <div class="col-lg-6">
+        <p> Seb <p>
+          <img src="/user/avatar.jpg" height="50" width="50">
+      </div>
+      <!-- colonne de droite avec les boutons de profil et de deconnexion -->
+      <div class="col-lg-6">
+        <button type="button" class="btn btn-info ">Profil</button>
+        <button type="button" class="btn btn-info ">Déconnexion</button>
+      </div>
+    </div>
   </div>
 </div>
