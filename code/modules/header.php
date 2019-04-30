13e54ea8a1f9ba7539bcdc5b16e1226ddc0f6477
seb <sebastien.poussard@protonmail.ch>
2019-04-30 13:09:06 +0200

    mise en forme du header

--- code/modules/header.php
+++ code/modules/header.php
@@ -3,34 +3,41 @@
   <!--  logo -->
   <div class="col-lg-2">
     <a href="index.php">
-      <img src="/img/logo.png" width="120" height="90">
+      <img src="/img/logo.png"class="img-fluid">
     </a>
   </div>
   <!-- Accueil & recherche -->
   <div class="col-lg-7">
-    <h3> Accueil <h3>
-    <!-- Search form -->
-    <div class="input-group mb-3">
-      <input type="text" class="form-control" placeholder="Rechercher" aria-label="Recipient's username" aria-describedby="basic-addon2">
-      <div class="input-group-append">
-        <button class="btn btn-outline-secondary" type="button">Rechercher</button>
+    <div class="">
+      <h3 class="text-center"> Accueil <h3>
+      <!-- Search form -->
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Rechercher" aria-label="Recipient's username" aria-describedby="basic-addon2">
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary" type="button">Rechercher</button>
+        </div>
       </div>
     </div>
   </div>
   <!-- cadre profil, se deconnecter ... -->
   <div class="col-lg-3">
     <!-- sous groupe avec la bordure -->
-    <div class="row border border-primary" style="margin:8px; border-radius:10px;">
-      <p> Connecté en tant que ...</p>
-      <!-- colonne de gauche avec nom d'utilisateur et avatar -->
-      <div class="col-lg-6">
-        <p> Seb <p>
-          <img src="/user/avatar.jpg" height="50" width="50">
+    <div class="row">
+      <!-- colonne de gauche avec l'avatar de l'utilisateur -->
+      <div class="col-lg-4">
+        <div class="align-middle">
+          <img  class="img-fluid align-middle" src="/user/avatar.jpg" style="    display: flex;
+    align-items: center;
+    flex-wrap: wrap;">
+        </div>
       </div>
-      <!-- colonne de droite avec les boutons de profil et de deconnexion -->
-      <div class="col-lg-6">
+      <!-- colonne de droite avec le nom de l'utilisateur et les boutons deconnecter et profil -->
+      <div class="col-lg-8">
+        <div class="align-middle">
+        <p class="text-center"> Seb <p>
         <button type="button" class="btn btn-info ">Profil</button>
         <button type="button" class="btn btn-info ">Déconnexion</button>
+        </div>
       </div>
     </div>
   </div>
