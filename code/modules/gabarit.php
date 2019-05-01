87978a589f1a09a5a8399458d542440c8e521848
seb <sebastien.poussard@protonmail.ch>
2019-05-01 18:53:37 +0200

    gabarit + header
    header :
        - refactoring en utilisant navbar
        - responsive design
    gabarit :
        - mise en forme avec image de background
        - modification de l'image pour être plus "claire"

--- code/modules/gabarit.php
+++ code/modules/gabarit.php
@@ -3,21 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <title>QuietCar, le covoit sans blabla</title>
+    <!-- css du site -->
+    <link rel="stylesheet" href="/css/main.css">
     <!-- bootstrap css -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- css d'open iconic compatible bootstrap -->
+    <link rel="stylesheet" href="/open-iconic/font/css/open-iconic-bootstrap.css">
 </head>
 <body>
 <?php
 require 'cobdd.php';
 require 'header.php';
  ?>
-<div class="row">
+<div class="row" id="t-max">
   <div class="col-lg-2">
     <!-- la colonne de gauche rechercher un trajet -->
     <?php require 'rechercher.php'; ?>
   </div>
-  <div class="col-lg-8">
+  <div class="col-lg-8" id="bg-img">
     <!-- contenu centrale à modifier en fonction de la page -->
+    <div id="background-image">
+    </div>
   </div>
   <div class="col-lg-2">
     <!-- colonne de droite pour proposer un trajet -->
--- code/modules/header.php
+++ code/modules/header.php
@@ -1,44 +1,36 @@
-<!-- le code pour la partie haute de l'application web -->
-<div class="row border border-primary" style="margin:8px; border-radius:10px;">
-  <!--  logo -->
-  <div class="col-lg-2">
-    <a href="index.php">
-      <img src="/img/logo.png"class="img-fluid">
-    </a>
+<!-- barre de navigation du site -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light py-0">
+  <!-- responsive design : si l'écran est trop petit un bouton apparait -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <!-- la "marque" du site, correspond au logo -->
+  <a class="navbar-brand" href="#">
+    <img src="/img/logo.png" width="80" height="50" alt="logo QuietCar">
+  </a>
+  <!-- les éléments de la barre de navigation -->
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <!-- formulaire de recherche -->
+    <form class="form-inline mx-auto">
+      <input class="form-control mr-sm-2" type="search" placeholder="Utilisateur" aria-label="Search">
+      <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">recherche</button>
+    </form>
+    <!-- partie droite de la barre de navigation -->
+    <ul class="nav navbar-nav navbar-right">
+      <!-- photo de l'utilisateur -->
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <img class="rounded" src="/user/avatar.jpg" width="50" height="50">
+        </a>
+      </li>
+      <!-- accès au profil de l'utilisateur -->
+      <li class="nav-item">
+        <a class="nav-link oi oi-person text-primary pt-3" href="#"> Sébastien</a>
+      </li>
+      <!-- déconnexion de l'utilisateur -->
+      <li class="nav-item">
+        <a class="nav-link oi oi-account-logout text-primary pt-3" href="#"> Déconnexion</a>
+      </li>
+    </ul>
   </div>
-  <!-- Accueil & recherche -->
-  <div class="col-lg-7">
-    <div class="">
-      <h3 class="text-center"> Accueil <h3>
-      <!-- Search form -->
-      <div class="input-group mb-3">
-        <input type="text" class="form-control" placeholder="Rechercher" aria-label="Recipient's username" aria-describedby="basic-addon2">
-        <div class="input-group-append">
-          <button class="btn btn-outline-secondary" type="button">Rechercher</button>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- cadre profil, se deconnecter ... -->
-  <div class="col-lg-3">
-    <!-- sous groupe avec la bordure -->
-    <div class="row">
-      <!-- colonne de gauche avec l'avatar de l'utilisateur -->
-      <div class="col-lg-4">
-        <div class="align-middle">
-          <img  class="img-fluid align-middle" src="/user/avatar.jpg" style="    display: flex;
-    align-items: center;
-    flex-wrap: wrap;">
-        </div>
-      </div>
-      <!-- colonne de droite avec le nom de l'utilisateur et les boutons deconnecter et profil -->
-      <div class="col-lg-8">
-        <div class="align-middle">
-        <p class="text-center"> Seb <p>
-        <button type="button" class="btn btn-info ">Profil</button>
-        <button type="button" class="btn btn-info ">Déconnexion</button>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+</nav>
