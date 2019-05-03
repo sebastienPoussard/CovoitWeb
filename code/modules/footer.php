66f8a407e0d97aacbeed35e198d5357b21c36dd5
seb <sebastien.poussard@protonmail.ch>
2019-05-03 10:27:12 +0200

    header + footer + gabarit
    gabarit : modification de la mise en page pour que l'image prenne en compte aussi les 2 colonnes rechercher et proposer
    
    footer : premiere essais

---
+++ code/modules/footer.php
@@ -0,0 +1,27 @@
+<!-- footer -->
+<footer>
+  <div class="footer">
+    <div class="row">
+      <div class="col-lg-3 text-center">
+        <a class="oi oi-envelope-closed" href="mailto:contact@covoit.pouseb.fr">
+          nous contacter
+        </a>
+      </div>
+      <div class="col-lg-6 text-center">
+        centre
+      </div>
+      <div class="col-lg-3 text-right">
+        <a class="oi oi-code" href="https://github.com/Qlaus">
+          Badr Riouch
+        </a>
+        <a class="oi oi-code" href="https://github.com/MelieMelie">
+          Marie-Amélie Mariscalco
+        </a>
+        <a class="oi oi-code" href="https://github.com/sebastienPoussard">
+          Sébastien Poussard
+        </a>
+      </div>
+
+    </div>
+  </div>
+</footer>
--- code/modules/gabarit.php
+++ code/modules/gabarit.php
@@ -15,14 +15,13 @@
 require 'cobdd.php';
 require 'header.php';
  ?>
-<div class="row" id="t-max">
+<div class="row" id="bg-img">
   <div class="col-lg-2">
     <!-- la colonne de gauche rechercher un trajet -->
     <?php require 'rechercher.php'; ?>
   </div>
-  <div class="col-lg-8" id="bg-img">
+  <div class="col-lg-8">
     <!-- contenu centrale à modifier en fonction de la page -->
-    <div id="background-image">
     </div>
   </div>
   <div class="col-lg-2">
@@ -36,4 +35,5 @@ require 'header.php';
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </body>
+<?php require 'footer.php'; ?>
 </html>
