c7e6983fff5bd42dbd4c534c7db1125f8a075064
MelieMelie <melieetmax@gmail.com>
2019-05-07 12:47:27 +0200

    ajout du module de upload une photo de profil
    bootstrap et css mis en place sur le formulaire
    l'ajout de photo de profil est maintenant possible depuis la page d'inscription

---
+++ code/Inscription/avatar.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: melie
+ * Date: 07/05/19
+ * Time: 10:21
+ */
+
+$avatar=$_FILES['avatar'];
+$cheminDuFichier='../user/';
+$poid =2000000;
+$largeur = 20000;
+$longueur = 20000;
+$ext = array('jpg', 'jpeg', 'gif', 'png'); //Liste des extensions valides
+$CalculTailleAvatar = getimagesize($avatar['tmp_name']);
+$verifExtension = pathinfo($avatar['name'], PATHINFO_EXTENSION);
+if ($avatar['size'] > $poid) {
+    echo "Le fichier est trop lourd";
+}
+else if ($CalculTailleAvatar[0] > $largeur OR $CalculTailleAvatar[1] > $longueur) {
+    echo "l'image n'a pas la bonne taille pour un avatar";
+}
+else if (!in_array($verifExtension, $ext)) {
+
+    echo "type de fichier non suporté";
+}
+else {
+    move_uploaded_file($avatar['tmp_name'],"../user/" . $avatar['name']);
+}
--- code/Inscription/inscription.php
+++ code/Inscription/inscription.php
@@ -6,13 +6,13 @@
  * Time: 15:56
  */
 include '../covoitPDO.php';
+
 $nomUser=$_POST['nomuser'];
 $prenomUser=$_POST['prenomuser'];
 $IdentifiantMail=$_POST['mail'];
 $motDePasse=$_POST['mdp'];
 $verifMotDePasse=$_POST['mdpVerif'];
 $descriptionUser=$_POST['description'];
-$avatar=$_FILES['avatar'];
 
 $verifIdentifiant=$bdd->prepare('SELECT COUNT(*) AS nbr FROM utilisateur WHERE mail=:identifiant');
 $verifIdentifiant->bindValue(':identifiant',$IdentifiantMail, PDO::PARAM_STR);
@@ -32,7 +32,6 @@ if (!preg_match("#^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]{2,}\.[a-z]{2,4}$#", $Identifia
 
 if ($motDePasse == $verifMotDePasse) {
     $cryptageMotDePasse = password_hash($_POST['mdp'], PASSWORD_DEFAULT);
-    echo $cryptageMotDePasse;
 }
 else {
     echo "vos mots de passe sont différents";
@@ -40,28 +39,6 @@ else {
 
 }
 
-//On définit les variables :
-$poid =2000000;
-$largeur = 100;
-$longueur = 100;
-$ext = array('jpg', 'jpeg', 'gif', 'png'); //Liste des extensions valides
-echo $avatar['size'];
-if ($avatar['size'] > $poid) {
-    echo "Le fichier est trop lourd";
-    exit();
-}
-$CalculTailleAvatar = getimagesize($avatar['size']);
-if ($CalculTailleAvatar[0] > $largeur OR $CalculTailleAvatar[1] > $longueur) {
-    echo "l'image n'a pas la bonne taille pour un avatar";
-    exit();
-}
-
-$verifExtension = pathinfo($avatar['name'], PATHINFO_EXTENSION);
-if (!in_array($verifExtension, $ext)) {
-
-    echo "type de fichier non suporté";
-    exit();
-}
 
 $verifIdentifiant=$bdd->prepare('INSERT INTO utilisateur (mail, mdp, nomuser,             
         prenomuser) VALUES (:mail, :mdp, :nomuser, :prenomuser)');
