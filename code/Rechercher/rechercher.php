d9dd174106ce491f98fd556d11a789f1b656fde6
MelieMelie <melieetmax@gmail.com>
2019-05-09 16:17:17 +0200

    ajout du module de recherche de covoiturage
    il reste à faire la mise en page de l'affichage des resultats de la recherche

---
+++ code/Rechercher/rechercher.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: melie
+ * Date: 09/05/19
+ * Time: 13:50
+ */
+include '../covoitPDO.php';
+$lieuDepart=$_POST['lieuDepart'];
+$lieuArrivee=$_POST['lieuArrivee'];
+$dateDepart=$_POST['dateDepart'];
+$heureDepart=$_POST['heureDepart'];
+$nombreBagage=$_POST['bagages'];
+$reqRechercher= $bdd->prepare('SELECT * FROM trajet WHERE pointdepart=:lieuDepart AND pointarrivee=:lieuArrivee');
+$reqRechercher->execute(
+    array(
+        "lieuDepart" => $lieuDepart,
+        "lieuArrivee" => $lieuArrivee,
+    )
+) or die(print_r($bdd->errorInfo()));
+
+$resultat = $reqRechercher->fetchAll(PDO::FETCH_ASSOC);
+
+if (!$resultat) {
+    echo'aucun covoiturage à cette date';
+}
+else{
+    foreach ($resultat as $trajet)
+        foreach ($trajet as $info)
+            echo $info . '<br>';
+}
+
+
