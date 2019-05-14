adef787679c1dd8c616b94f19d27c1fe6d17668b
seb <sebastien.poussard@protonmail.ch>
2019-05-14 15:38:59 +0200

    fix erreur integration

--- code/modules/gabaritMillieu.php
+++ code/modules/gabaritMillieu.php
@@ -7,6 +7,6 @@
   <div class="row" id="bg-img">
     <div class="col-lg-2">
       <!-- la colonne de gauche rechercher un trajet -->
-      <?php require $_SERVER["DOCUMENT_ROOT"].'/Rechercher/rechercher.html'; ?>
+      <?php require $_SERVER["DOCUMENT_ROOT"].'/modules/rechercher.html'; ?>
     </div>
     <div class="col-lg-8">
