6ad333d37fb861eaeb80d3e38e71e50cc78bd498
Badr <badr.riouch@yahoo.fr>
2019-05-20 23:56:14 +0200

    confirmationcovoiturage 0.63

--- code/insertioncovoitBDD.php
+++ code/insertioncovoitBDD.php
@@ -5,10 +5,10 @@
  * Date: 20/05/2019
  * Time: 23:04
  */
-
+//var_dump($_SESSION);
 if(isset($_POST['button']))
 {
-    $reqInsert= 'INSERT INTO Trajet(idTrajet , pointdepart , pointarrivee , dateheuredepart, conducteur) VALUES('.$_SESSION['$idTrajet'].','.$_SESSION['$lieuDepart'].','.$_SESSION['$lieuarrivee'].','.$_SESSION['$dateHeureDepart'].','.$_SESSION['identifiant'];
+    $reqInsert= 'INSERT INTO Trajet(idTrajet , pointdepart , pointarrivee , dateheuredepart, conducteur) VALUES('.$_SESSION['$idTrajet'].','.$_SESSION['$lieuDepart'].','.$_SESSION['$lieuarrivee'].','.$_SESSION['$dateHeureDepart'].','.$_SESSION['identifiant'].')';
     $conn->execute($reqInsert);
     header("Location: https://covoit.pouseb.fr/index.php");
 }
