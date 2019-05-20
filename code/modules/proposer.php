00db4037034d039e60cbaec0f36a20a99c5b5c30
Badr <badr.riouch@yahoo.fr>
2019-05-20 20:51:29 +0200

    premier essai d'integration de proposition covoiturage

--- code/modules/proposer.php
+++ code/modules/proposer.php
@@ -2,7 +2,7 @@
   <div class="text-center">
     <h2>Proposer</h2>
   </div>
-  <form method="post" action="../Proposer/confirmationcovoiturage.php" enctype="multipart/form-data">
+  <form method="post" action="/confirmationcovoiturage.php" enctype="multipart/form-data">
     <p><input type="text" name="depart" placeholder="De:" required class="form-control"></p>
     <p><input type="text" name="arrivee" placeholder="à" required class="form-control"></p>
     <p><input type="datetime-local" name="dateHeureDepart" placeholder="Jour" required class="form-control"></p>
