1004976512284ad4e706d1a01da027797b6acd0d
seb <sebastien.poussard@protonmail.ch>
2019-05-21 17:42:39 +0200

    upgrade mes reservations
    affichage en fonction de si la reservation à été acceptée ou non

--- code/mes_reservations.php
+++ code/mes_reservations.php
@@ -21,18 +21,39 @@ if ($_SESSION['identifiant']) {
     $req2->execute(array('idTrajet'=> $trajet['idtrajet']));
     $res2 = $req2->fetchAll(PDO::FETCH_ASSOC);
 
+    // savoir si le trajet à été accepté par le Conducteur
+    $reqConf = $bdd->prepare('SELECT estaccepte FROM reservation
+                              WHERE mail = :mail
+                              AND idtrajet = :idtrajet;');
+    $toto = $reqConf->execute(array('idtrajet'=> $trajet['idtrajet'],
+                            'mail'=> $_SESSION['identifiant']));
+    $reqConf = $reqConf->fetchAll(PDO::FETCH_ASSOC);
+    $reqConf[0]['estaccepte'];
     // convertir la date en format fr
     $datefr = strftime("%d/%m/%Y à %H:%M",strtotime($res2[0]['dateheuredepart']));
 
     echo '
-     <div class="card text-center">
+     <div class="card text-center ';
+     if ($reqConf[0]['estaccepte']) {
+       echo 'card border-success';
+     } else {
+       echo 'car border-danger';
+     }
+     echo '
+     ">
       <div class="card-header oi oi-flag">
         Trajet de '.$res2[0]['pointdepart'].' à '.$res2[0]['pointarrivee'].'
       </div>
       <div class="card-body">
         <h5 class="card-title oi oi-clock"> Départ prévue : '.$datefr.'</h5>
         <p class="card-text">Immatriculation du véhicule : '.$res2[0]['idvoiture'].'</p>
-        <p class="card-text oi oi-envelope-closed"> Contacter le conducteur : '.$res2[0]['conducteur'].'</p>
+        <p class="card-text oi oi-envelope-closed"> Contacter le conducteur : '.$res2[0]['conducteur'].'</p>';
+        if (!$reqConf[0]['estaccepte']) {
+          echo '<p class="card-text text-danger">Le conducteur n\'à pas encore acceptée votre reservation </p>';
+        } else {
+          echo '<p class="card-text text-success">Votre reservation à été acceptée par le conducteur</p>';
+        }
+      echo '
       </div>
       <div class="card-footer text-muted">
       <form class="" action="mes_reservations.php" method="post">
