d0f7e65562d14a6cb99d7a023da93584e0f139ca
Badr <badr.riouch@yahoo.fr>
2019-05-21 17:44:48 +0200

    ajoutvoiture 0.9

--- code/ajoutVoiture.php
+++ code/ajoutVoiture.php
@@ -11,7 +11,7 @@ echo "<form method='post' action='/insertionvoitureBDD.php' enctype='multipart/f
         echo "<tr> <td><input type='text' name='modele' placeholder='Modèle' required class='form-control'></td> </tr>";
         echo "<tr> <td><input type='text' name='matricule' placeholder='Matricule' required class='form-control'></td> </tr>";
         echo "<tr> <td><input type='number' name='maxbagages' min='1' max='50' placeholder='Max. bagages' required class='form-control'></td> </tr>";
-        echo "<tr> <td><input type='number' name='maxpassages' min='1' max='50' placeholder='Max. passagers' required class='form-control'></td> </tr>";
+        echo "<tr> <td><input type='number' name='maxpassagers' min='1' max='50' placeholder='Max. passagers' required class='form-control'></td> </tr>";
         echo "<tr> <td> <input type='submit' class='btn btn-info' name='valider' value='Ajouter' id='button'> </td> </tr>";
     echo "</table>";
 echo "</form>";
