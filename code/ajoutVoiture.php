66f3e433a3342f05a26a872d0bfc68e647c5a177
Badr <badr.riouch@yahoo.fr>
2019-05-22 19:52:31 +0200

    ajoutVoiture 0.95

--- code/ajoutVoiture.php
+++ code/ajoutVoiture.php
@@ -9,13 +9,13 @@ $mail = $_SESSION['identifiant'];
  */
 echo "<form method='post' action='insertionvoitureBDD.php' enctype='multipart/form-data'>";
     echo "<table class='table table-hover'>";
-        echo "<tr> <th> Nouvelle voiture de : </th><th> $mail </th> </tr>";
-        echo "<tr> <td colspan='2'><input type='text' name='marque' placeholder='Marque' required class='form-control'></td> </tr>";
-        echo "<tr> <td colspan='2'><input type='text' name='modele' placeholder='Modèle' required class='form-control'></td> </tr>";
-        echo "<tr> <td colspan='2'><input type='text' name='matricule' placeholder='Matricule' required class='form-control'></td> </tr>";
-        echo "<tr> <td colspan='2'><input type='number' name='maxbagages' min='1' max='50' placeholder='Max. bagages' required class='form-control'></td> </tr>";
-        echo "<tr> <td colspan='2'><input type='number' name='maxpassagers' min='1' max='50' placeholder='Max. passagers' required class='form-control'></td> </tr>";
-        echo "<tr> <td colspan='2'> <input type='submit' class='btn btn-info' name='valider' value='Ajouter' id='button'> </td> </tr>";
+        echo "<tr> <th> Nouvelle voiture de : $mail </th> </tr>";
+        echo "<tr> <td><input type='text' name='marque' placeholder='Marque' required class='form-control'></td> </tr>";
+        echo "<tr> <td><input type='text' name='modele' placeholder='Modèle' required class='form-control'></td> </tr>";
+        echo "<tr> <td><input type='text' name='matricule' placeholder='Matricule' required class='form-control'></td> </tr>";
+        echo "<tr> <td><input type='number' name='maxbagages' min='1' max='50' placeholder='Max. bagages' required class='form-control'></td> </tr>";
+        echo "<tr> <td><input type='number' name='maxpassagers' min='1' max='50' placeholder='Max. passagers' required class='form-control'></td> </tr>";
+        echo "<tr> <td> <input type='submit' class='btn btn-info' name='valider' value='Ajouter' id='button'> </td> </tr>";
     echo "</table>";
 echo "</form>";
 
