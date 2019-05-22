e477a8d9d6661a493b2590c10255da79bfae2eb4
Badr <badr.riouch@yahoo.fr>
2019-05-22 17:15:02 +0200

    mise en page d'index (test)

--- code/index.php
+++ code/index.php
@@ -8,12 +8,23 @@ $request = $bdd->prepare($sql);
 $request->execute();
 while($result=$request->fetch())
 {
-    echo "<table>";
-    echo "<tr><td colspan='3'>trajet du ".$result['dateheuredepart']."</tr></td>";
-    echo "<tr><td>".$result['pointdepart']."</td><td>-></td><td>".$result['pointarrivee']."</td></tr>";
-    if($result['estannule']==true)
-    {echo "<tr><td colspan='3'>ANNULÉ</tr></td>";}
-    echo "</table>";
+    echo '<div class="card text-center">';
+        echo '<div class="card-header oi oi-flag">';
+            echo 'Trajet de '.$trajet['pointdepart'].' à '.$trajet['pointarrivee'].;
+        echo '</div>';
+        echo '<div class="card-body">';
+            echo '<div class="row">';
+                echo '<div class="col-lg-3">';
+                    echo '<img class="" src="/user/'.$trajet['conducteur'].'.jpg" width="100" height="100" alt="Card image cap">';
+                echo '</div>';
+                echo '<div class="col-lg-9">';
+                    echo '<h5 class="card-title oi oi-clock"> Départ prévue : '.$datefr.'</h5>';
+                    echo '<p class="card-text">Immatriculation du véhicule : '.$trajet['idvoiture'].'</p>';
+                    echo '<p class="card-text oi oi-envelope-closed"> Conducteur : '.$trajet['conducteur'].'</p>';
+                echo '</div>';
+            echo '</div>';
+        echo '</div>';
+    echo '</div>';
 }
 ?>
 
