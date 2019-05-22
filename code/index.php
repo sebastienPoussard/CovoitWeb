5fe53aee8d14b9a67a8395cc2f3062ff3b7c8f98
Badr <badr.riouch@yahoo.fr>
2019-05-22 15:29:22 +0200

    index

--- code/index.php
+++ code/index.php
@@ -6,8 +6,15 @@
 $sql= 'SELECT * FROM trajet ORDER BY idtrajet DESC ';// Prepare and execute the query
 $request = $bdd->prepare($sql);
 $request->execute();
-
-var_dump($request);
+while($result=$request->fetch())
+{
+    echo "<table>";
+    echo "<tr><td colspan='3'>trajet du ".$result['dateheuredepart']."</tr></td>";
+    echo "<tr><td>".$result['pointdepart']."</td><td>-></td><td>".$result['pointarrivee']."</td></tr>";
+    if($result['estannule']==true)
+    {echo "<tr><td colspan='3'>ANNULÉ</tr></td>";}
+    echo "</table>";
+}
 ?>
 
 <?php require $_SERVER["DOCUMENT_ROOT"].'/modules/gabaritFin.php'; ?>
