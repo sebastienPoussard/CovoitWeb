f435cb0b999ec2c579cb8b9c3c62be951880b401
Badr <badr.riouch@yahoo.fr>
2019-05-22 17:27:23 +0200

    index 0.92

--- code/index.php
+++ code/index.php
@@ -8,6 +8,8 @@ $request = $bdd->prepare($sql);
 $request->execute();
 while($result=$request->fetch())
 {
+    $photo_profil='/user/'.$result['conducteur'].'.jpg';
+
     echo '<div class="card text-center">';
         echo '<div class="card-header oi oi-flag">';
             echo 'Trajet de '.$result['pointdepart'].' à '.$result['pointarrivee'];
@@ -15,16 +17,26 @@ while($result=$request->fetch())
         echo '<div class="card-body">';
             echo '<div class="row">';
                 echo '<div class="col-lg-3">';
-                    echo '<img class="" src="/user/'.$result['conducteur'].'.jpg" width="100" height="100" alt="Card image cap">';
+                if(file_exists($photo_profil))
+                {
+                    echo '<img class="" src='.$photo_profil.' width="100" height="100" alt="Card image cap">';
+                }
+                else
+                {
+                    echo '<img class="" src="/img/logoPhotodeProfil.png" width="100" height="100" alt="Card image cap">';
+                }
+
                 echo '</div>';
                 echo '<div class="col-lg-9">';
-                    echo '<h5 class="card-title oi oi-clock"> Départ prévue : '.$result['dateheuredepart'].'</h5>';
+                    echo '<h5 class="card-title oi oi-clock"> Départ prévu : '.$result['dateheuredepart'].'</h5>';
                     echo '<p class="card-text">Immatriculation du véhicule : '.$result['idvoiture'].'</p>';
                     echo '<p class="card-text oi oi-envelope-closed"> Conducteur : '.$result['conducteur'].'</p>';
                 echo '</div>';
             echo '</div>';
         echo '</div>';
     echo '</div>';
+
+    echo '<br>';
 }
 ?>
 
